1eef6a5f00a2912139dd855879b9dbc02edc29ab
tihorskiialeks <105417398+tihorskiialeks@users.noreply.github.com>
2022-11-04 06:54:08 +0200

    weather output updated

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/helpers/data_service.dart';
 import 'package:weather_app/helpers/weather_response.dart';
+import 'package:intl/intl.dart';
 
 import '../widgets/user_input.dart';
 import '../widgets/weather_output.dart';
@@ -46,11 +47,14 @@ class _HomePageState extends State<HomePage> {
                     child: SizedBox(
                       width: MediaQuery.of(context).size.width * 0.65,
                       child: TextField(
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
                           controller: _cityTextController,
                           decoration: const InputDecoration(
                             hintText: 'Start typing for search',
+                            hintStyle: TextStyle(fontSize: 18),
                             border: InputBorder.none,
-
                           )),
                     ),
                   ),
@@ -76,8 +80,8 @@ class _HomePageState extends State<HomePage> {
               width: MediaQuery.of(context).size.width * 0.85,
               height: MediaQuery.of(context).size.height * 0.43,
               decoration: BoxDecoration(
-                color: Colors.white70,
-              ),
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.circular(50)),
               child: Column(
                 children: [
                   if (_response != null)
@@ -85,41 +89,123 @@ class _HomePageState extends State<HomePage> {
                       children: [
                         Row(
                           children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.26,
-                              height: MediaQuery.of(context).size.height * 0.15,
-                              child: Image.network(_response!.iconUrl),
-                              decoration: BoxDecoration(color: Colors.white),
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.26,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.13,
+                                child: Image.network(_response!.iconUrl),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(40)),
+                              ),
                             ),
                             Column(
                               children: [
-                                Text('${_response!.tempInfo.temperature} C'),
-                                Text('${_response!.weatherInfo.main}'),
-                                Text('${_response!.weatherInfo.description}'),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 8.0, bottom: 8),
+                                  child: Text(
+                                    '${_response!.tempInfo.temperature} \u2103',
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8),
+                                  child: Text(
+                                    '${_response!.weatherInfo.main}',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                                Text('${_response!.weatherInfo.description}',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Color(0xff9E9E9E),
+                                    )),
                               ],
                             ),
                           ],
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                                '${_response!.cityName}, ${_response!.countryInfo.country}'),
-                            Text('Date today'), // todo
-                          ],
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20.0),
+                                child: Text(
+                                  '${_response!.cityName}, ${_response!.countryInfo.country}',
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 20.0),
+                                child: Text(
+                                  DateFormat('dd MMMM yyyy')
+                                      .format(DateTime.now()),
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xff9E9E9E),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.03,
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             Column(
                               children: [
-                                const Text('Min'),
-                                Text('${_response!.tempInfo.minTemp} C')
+                                const Text(
+                                  'Min',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xff9E9E9E),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10.0),
+                                  child: Text(
+                                    '${_response!.tempInfo.minTemp} \u2103',
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xff212121),
+                                    ),
+                                  ),
+                                )
                               ],
                             ),
                             Column(children: [
-                              const Text('Max'),
-                              Text('${_response!.tempInfo.maxTemp} C')
+                              const Text(
+                                'Max',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xff9E9E9E),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10.0),
+                                child: Text('${_response!.tempInfo.maxTemp} \u2103',
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xff212121),
+                                  ),),
+                              )
                             ]),
                           ],
                         ),
